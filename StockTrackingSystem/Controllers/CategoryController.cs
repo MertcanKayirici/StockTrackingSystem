7e57f1e5f589a032ddfed37e3b040dd6b078f126
Mertcan <mertcankayirici@gmail.com>
2026-03-22 03:30:18 +0300

    Add category listing page using Entity Framework

---
+++ StockTrackingSystem/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockTrackingSystem.Data;
+using System.Linq;
+
+namespace StockTrackingSystem.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
